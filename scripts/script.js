df4756bbae1726a549ef1cdba5480d7ba94d71d8
@EkaHvo <ekasagalaeva@gmail.com>
2022-06-01 14:17:09 +0300

    feat: add close button with script

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+function burgerMenu() {
+  let menu = document.querySelector(".nav-list");
+  let links = document.querySelector(".nav-list__item");
+  let button = document.querySelector(".burger-button");
+  let burger = document.querySelector(".burger");
+  // let overlay = menu.find('.burger-menu_overlay');
+  
+  
+  button.addEventListener('click', () => toggleMenu());
+  menu.addEventListener('click', () => toggleMenu());
+  
+  function toggleMenu(){
+    burger.classList.toggle('active');
+    menu.classList.toggle('active');
+    
+  }
+}
+
+burgerMenu();
