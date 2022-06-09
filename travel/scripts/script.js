7d8b72cf7e7764ce8fa9c75764d649fd56f96f6e
@EkaHvo <ekasagalaeva@gmail.com>
2022-06-09 09:08:40 +0300

    feat: fix footer

---
+++ travel/scripts/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+  let dotsArr = document.querySelectorAll('.dot');
+  dotsArr.forEach(dot => {
+    dot.addEventListener('click', ()=>{
+      
+    })
+  })
+
+});
