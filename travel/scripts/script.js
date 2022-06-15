f3d758460b310b610d5eee5e951b7a6e8f2aecdb
@EkaHvo <ekasagalaeva@gmail.com>
2022-06-15 10:55:40 +0300

    feat: add console info

--- travel/scripts/script.js
+++ travel/scripts/script.js
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     dot.addEventListener('click', ()=>{
       let id = dot.dataset.slide;
 
-      slideItems(id)
+      slideItems(id);
     })
   })
 
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     id > slidesArr.length ? id = id - slidesArr.length : '';
 
-    slideItems(id)
+    slideItems(id);
   })
 
   const preArrow = slider.querySelector('.pre');
@@ -81,7 +81,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     id < 1 ? id = slidesArr.length - id  : '';
 
-    slideItems(id)
+    slideItems(id);
   })
 
   function moveSlider(id){
@@ -110,10 +110,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
   function removeActive(){
     slidesArr.forEach(slide => {
       slide.classList.remove('active');
-    })
+    });
     dotsArr.forEach(dot => {
       dot.classList.remove('active');
-    })
+    });
   }
 
   function slideItems(id){
@@ -123,5 +123,40 @@ document.addEventListener("DOMContentLoaded", ()=>{
   }
 
   slideItems(2);
+
+  console.log(
+    `
+    100/100
+    1. Вёрстка валидная +10
+    2. Вёрстка семантическая +20
+        В коде странице присутствуют следующие элементы (указано минимальное количество, может быть больше):
+        <header>, <main>, <footer> +3
+        четыре элемента <section> (по количеству секций) +3
+        только один заголовок <h1> +3
+        три заголовка <h2> (количество секций минус одна, у которой заголовок <h1>) +3
+        один элемент <nav> (панель навигации) +3
+        два списка ul > li > a (панель навигации, ссылки на соцсети) +3
+        четыре кнопки <button> +2
+    3. Вёрстка соответствует макету +48
+        блок <header> +6
+        секция preview +9
+        секция steps +9
+        секция destinations +9
+        секция stories +9
+        блок <footer> +6
+    4. Требования к css + 12
+        для построения сетки используются флексы или гриды +2
+        при уменьшении масштаба страницы браузера вёрстка размещается по центру, а не сдвигается в сторону +2
+        фоновый цвет тянется на всю ширину страницы +2
+        иконки добавлены в формате .svg. SVG может быть добавлен любым способом. Обращаем внимание на формат, а не на способ добавления +2
+        изображения добавлены в формате .jpg +2
+        есть favicon +2
+    5. Интерактивность, реализуемая через css +20
+        плавная прокрутка по якорям +5
+        ссылки в футере ведут на гитхаб автора проекта и на страницу курса https://rs.school/js-stage0/ +5
+        интерактивность включает в себя не только изменение внешнего вида курсора, например, при помощи свойства cursor: pointer, но и другие визуальные эффекты, например, изменение цвета фона или цвета шрифта.  +5
+        обязательное требование к интерактивности: плавное изменение внешнего вида элемента при наведении и клике не влияющее на соседние элементы +5
+    `
+  );
 
 });
